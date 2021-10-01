4eb4065ecb38d2cf8edcf5ca468e17e5fb5f34d3
shreetiranjit <ranjit.shreeti1@gmail.com>
2021-10-01 21:34:10 +0545

    tiktactoe

--- tiktactoe.py
+++ tiktactoe.py
@@ -1,224 +1,178 @@
 import pygame
 import sys
-import numpy as np
+from pygame.locals import *
 import time
-
-# initializing pygame
-pygame.init()
 pygame.mixer.init()
 
-width = 600
-heigth = 600
-line_width = 10
-board_rows = 3
-board_column = 3
-circle_radius = 60
-circle_width = 15
-cross_width = 25
-# space = 55
+# initializing global constants
+RM = 'x'
+winner = None
+draw = False
+width = 400
+height = 400
+white = (255, 255, 255)
+line_color = (10,10,10)
 
-# rgb
-screen_colour = (255, 255, 255)
-bg_color = (0, 0, 0)
-line_color = (255,255,255)
-color = (255,0,0)
+#initializing pygame window
+pygame.init()
+fps = 30
+CLOCK = pygame.time.Clock()
 
-# creating screen
-screen = pygame.display.set_mode((width , heigth))
-pygame.display.set_caption('TIC - TAC - TOE (RICK AND MORTY)')
-look_1 = pygame.image.load("C:/Users/Lenovo/Downloads/rickrackroe.jpg").convert_alpha()
-background = pygame.transform.smoothscale(look_1, (width, heigth))
-
-# board
-board = np.zeros((board_rows, board_column))
-
-# music
-red_circle = pygame.mixer.Sound("C:/Users/Lenovo/Downloads/hitler.mp3")
-win = pygame.mixer.Sound("C:/Users/Lenovo/Downloads/heckyeah.mp3")
+screen = pygame.display.set_mode((width, height+100),0,32)
+pygame.display.set_caption("Tic Tac Toe (Rick - And - Morty )")
 
 
-def draw_lines():
-    # first_horizontal_line
-    pygame.draw.line(background, line_color, (0,200), (600, 200),line_width )
-    # second_horizontal_line
-    pygame.draw.line(background, line_color, (0, 400), (600, 400), line_width)
 
-    # first_vertical_line
-    pygame.draw.line(background, line_color, (200, 0), (200, 600), line_width)
-    # second_vertical_line
-    pygame.draw.line(background, line_color, (400, 0), (400, 600), line_width)
+# 3x3 board
+RMRM = [[None]*3,[None]*3,[None]*3]
 
+# loading the images
+opening = pygame.image.load('C:/Users/Lenovo/Downloads/rickrackroe.jpg')
+morty_head_img = pygame.image.load('C:/Users/Lenovo/Downloads/morty_head.jpg')
+rick_head_img = pygame.image.load('C:/Users/Lenovo/Downloads/rick_head.jpg')
+# resizing images
+x_img = pygame.transform.scale(morty_head_img, (80,80))
+o_img = pygame.transform.scale(rick_head_img, (80,80))
+opening = pygame.transform.scale(opening, (width, height+100))
 
-def draw_figures():
-    for row in range(board_rows):
-        for col in range(board_column):
-            if board[row][col] == 1:
-                pygame.draw.circle(background, line_color, (int(col * 200 + 100), int(row * 200 + 100)),
-                                   circle_radius, circle_width)
-            elif board[row][col] == 2:
-                pygame.draw.circle(background, color, (int(col * 200 + 100), int(row * 200 + 100)),
-                                   circle_radius, circle_width)
+def game_opening():
+    screen.blit(opening,(0, 0))
+    pygame.display.update()
+    time.sleep(1)
+    screen.blit(opening, (0, 0))
+    # Drawing vertical lines
+    pygame.draw.line(screen, line_color, (width/3, 0), (width/3, height), 7)
+    pygame.draw.line(screen, line_color, (width/3*2, 0), (width/3*2, height), 7)
+    # Drawing horizontal lines
+    pygame.draw.line(screen, line_color, (0,height/3), (width, height/3), 7)
+    pygame.draw.line(screen, line_color, (0,height/3*2), (width, height/3*2), 7)
+    draw_status()
 
-
-def mark_square(row, col, player):
-    board[row][col] = player
-
-
-def available_square(row,col):
-    if board[row][col] == 0:
-        return True
+def draw_status():
+    global draw
+    if winner is None:
+        message = RM.upper() + "'s Turn"
     else:
-        return False
+        message = winner.upper() + " won!"
+    if draw:
+        message = 'Game Draw!'
+    font = pygame.font.Font(None, 30)
+    text = font.render(message, 1, (255, 255, 255))
+    # copy the rendered message onto the board
+    screen.fill ((0, 0, 0), (0, 400, 500, 100))
+    text_rect = text.get_rect(center=(width/2, 500-50))
+    screen.blit(text, text_rect)
+    pygame.display.update()
+
+def check_win():
+    global TTT, winner,draw
+    # check for winning rows
+    for row in range (0,3):
+        if ((RMRM[row][0] == RMRM[row][1] == RMRM[row][2]) and(RMRM[row][0] is not None)):
+            # this row won
+            winner = RMRM[row][0]
+            pygame.draw.line(screen, (250, 0, 0), (0, (row + 1)*height/3 - height/6), \
+                              (width, (row + 1)*height/3 - height/6), 4)
+            break
+
+    # check for winning columns
+    for col in range (0, 3):
+        if (RMRM[0][col] == RMRM[1][col] == RMRM[2][col]) and (RMRM[0][col] is not None):
+            # this column won
+            winner = RMRM[0][col]
+            #draw winning line
+            pygame.draw.line (screen, (250,0,0),((col + 1)* width/3 - width/6, 0), \
+                          ((col + 1)* width/3 - width/6, height), 4)
+            break
+    # check for diagonal winners
+    if (RMRM[0][0] == RMRM[1][1] == RMRM[2][2]) and (RMRM[0][0] is not None):
+        # game won diagonally left to right
+        winner = RMRM[0][0]
+        pygame.draw.line (screen, (250,70,70), (50, 50), (350, 350), 4)
+    if (RMRM[0][2] == RMRM[1][1] == RMRM[2][0]) and (RMRM[0][2] is not None):
+        # game won diagonally right to left
+        winner = RMRM[0][2]
+        pygame.draw.line (screen, (250,70,70), (350, 50), (50, 350), 4)
+    if(all([all(row) for row in RMRM]) and winner is None ):
+        draw = True
+    draw_status()
+
+def drawXO(row,col):
+    global RMRM,RM
+    if row==1:
+        posx = 30
+    if row==2:
+        posx = width/3 + 30
+    if row==3:
+        posx = width/3*2 + 30
+    if col==1:
+        posy = 30
+    if col==2:
+        posy = height/3 + 30
+    if col==3:
+        posy = height/3*2 + 30
+    RMRM[row-1][col-1] = RM
+    if(RM == 'x'):
+        screen.blit(x_img,(posy,posx))
+        RM = 'o'
+    else:
+        screen.blit(o_img,(posy,posx))
+        RM = 'x'
+    pygame.display.update()
 
 
-def is_board_full():
-    for row in range(board_rows):
-        for column in range(board_column):
-            if board[row][column] == 0 :
-                return False
-    else :
-        return True
+def userClick():
+    # get coordinates of mouse click
+    x, y = pygame.mouse.get_pos()
+    # get column of mouse click (1-3)
+    if x < width/3:
+        col = 1
+    elif x < width/3*2:
+        col = 2
+    elif x < width:
+        col = 3
+    else:
+        col = None
+    # get row of mouse click (1-3)
+    if y < height/3:
+        row = 1
+    elif y < height /3*2:
+        row = 2
+    elif y< height:
+        row = 3
+    else:
+        row = None
+
+    if row and col and RMRM[row-1][col-1] is None :
+        global RM
+        #draw the x or o on screen
+        drawXO(row, col)
+        check_win()
+
+def reset_game():
+    global RMRM, winner,RM, draw
+    time.sleep(1)
+    RM = 'x'
+    draw = False
+    game_opening()
+    winner=None
+    RMRM = [[None]*3,[None]*3,[None]*3]
 
 
-def check_win(player):
-    # vertical win check
-    for col in range(board_column):
-        if board[0][col] == player and board[1][col] == player and board[2][col] == player:
-            draw_vertical_line_winning(col, player)
-            return True
+game_opening()
+# run the game loop forever
+while True:
 
-    # horizontal win check
-    for row in range(board_column):
-        if board[row][0] == player and board[row][1] == player and board[row][2] == player:
-            draw_horizontal_line_winning(row, player)
-            return True
-
-    # ascending diagonal win check
-    if board[2][0] == player and board[1][1] == player and board[0][2] == player:
-        draw_asc_diagonal_line_winning(player)
-        return True
-
-    # descending diagonal win check
-    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
-        draw_des_diagonal_line_winning(player)
-        return True
-
-    return False
-
-
-def draw_vertical_line_winning(col, player):
-    posX = col * 200 + 100
-    if player == 1:
-        color = line_color
-    elif player == 2:
-        color = line_color
-
-    pygame.mixer.Sound.play(win)
-    pygame.draw.line(background, screen_colour, (posX, 15) , (posX, heigth - 15),15)
-    pygame.mixer.music.stop()
-
-
-
-
-def draw_horizontal_line_winning(row, player):
-    posY = row *200 + 100
-    if player == 1:
-        color = line_color
-    elif player == 2:
-        color = line_color
-
-    pygame.mixer.Sound.play(win)
-    pygame.draw.line(background, screen_colour, (15,posY), (width-15 , posY), 15)
-    pygame.mixer.music.stop()
-
-
-
-def draw_asc_diagonal_line_winning(player):
-    if player == 1:
-        color = line_color
-    elif player == 2:
-        color = line_color
-    pygame.mixer.Sound.play(win)
-    pygame.draw.line(background, screen_colour, (15, heigth - 15), (width - 15, 15), 15)
-    pygame.mixer.music.stop()
-
-
-
-def draw_des_diagonal_line_winning(player):
-    if player == 1:
-        color = line_color
-    elif player == 2:
-        color = line_color
-    pygame.mixer.Sound.play(win)
-    pygame.draw.line(background, screen_colour, (15, 15), (width - 15, heigth - 15), 15)
-    pygame.mixer.music.stop()
-
-
-
-
-def text_object(text, font):
-    textSurface = font.render(text, True, (0, 0, 0))
-    return textSurface, textSurface.get_rect()
-
-
-def restart():
-    time.sleep(3)
-    XO = 'x'
-    game_over = False
-    main()
-    # screen.blit(background, (0,0))
-    # draw_lines()
-    # # player = 1
-    # for row in range(board_rows):
-    #     for col in range(board_column):
-    #         board[row][col] = 0
-
-
-draw_lines()
-
-
-
-def main():
-    player = 1
-    game_over = False
-    # game loop
-    while True:
-        screen.blit(background, (0, 0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-            if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
-                mouseX = event.pos[0]  # x
-                mouseY = event.pos[1]  # y
-
-                clicked_row = int(mouseY // 200)
-                clicked_col = int(mouseX // 200)
-
-                if available_square(clicked_row, clicked_col):
-                    if player == 1:
-                        mark_square(clicked_row, clicked_col, 1)
-                        if check_win(player):
-                            game_over = True
-
-
-                        player = 2
-
-                    elif player == 2:
-                        pygame.mixer.Sound.play(red_circle)
-                        mark_square(clicked_row, clicked_col, 2)
-                        if check_win(player):
-                             game_over = True
-
-
-                        player = 1
-                        pygame.mixer.music.stop()
-                    draw_figures()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    restart()
-
-        pygame.display.update()
-
-
-main()
+    for event in pygame.event.get():
+        pygame.mixer.music.load('C:/Users/Lenovo/Downloads/theme.mp3')
+        pygame.mixer.music.play()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == MOUSEBUTTONDOWN:
+            # the user clicked; place an X or O
+            userClick()
+            if winner or draw :
+                reset_game()
+    pygame.display.update()
+    CLOCK.tick(fps)
